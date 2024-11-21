84694a6732d7987fbd470ba204b6957716fa078c
melvin-pointdexter <101175724+melvin-pointdexter@users.noreply.github.com>
2024-11-21 17:32:30 +0200

    refactor: change sorting algorithm to merge sort

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         WorkerManager manager = new WorkerManager(WorkerManager.workerCreator(10000));
 
-        //System.out.println("====================================\nThe workers before the sorting:\n");
+        //System.out.println("====================================\nThe workers before the sorting:\n" + manager);
         manager.sort();
-        //System.out.println("====================================\nThe workers after the sorting:\n" );
+        //System.out.println("====================================\nThe workers after the sorting:\n" + manager );
     }
 }
